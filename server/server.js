f320e2c48f66f0bc1d5c32eade6b356ed0d97d38
Raja Gopal Gajula <gajularajagopal03@gmail.com>
2025-06-20 22:34:02 +0530

    Fix:mounted api routes correctly

--- server/server.js
+++ server/server.js
@@ -7,16 +7,22 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const port = 4445 || process.env.PORT;
 
 app.use(express.json());
+
+const apiRoutes = require("./routes/apiRoutes");
+
+app.use("/api",apiRoutes);
 
 // DB Initialization
 mongoose.connect('mongodb+srv://rajagopalgajula520:raja123@cluster0.swlko.mongodb.net/sguShop?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>console.log('DB Connected'))
 .catch((e)=>console.log(e));
 
+// Default ROute
 app.get('/', (req, res) => {
   res.send('Welcome to SGU Shop API!');
 });
